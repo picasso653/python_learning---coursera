1498aeead940c55bfb7504eddcba791bf4adcecb
Isaac Korankye Asante <xquisit31@gmail.com>
2024-01-07 01:16:48 +0000

    python programming

---
+++ Module-1/python_input_output.py
@@ -0,0 +1,7 @@
+number = input('please enter a number: ')
+print(number)
+
+name = input('please enter your first name: ')
+surname = input('please enter your second name: ')
+
+print('Hello there, {} {}'.format(name, surname))
