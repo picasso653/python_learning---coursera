c5c9ccb67652d4021ec4d18417d5ad8490fdacfd
Isaac Korankye Asante <xquisit31@gmail.com>
2024-01-13 15:04:13 +0000

    File handling

---
+++ module-2/file handling/python_file_handling.py
@@ -0,0 +1,3 @@
+with open('module-2/file handling/testing.txt', mode= 'r') as file:
+    data =  file.readline()
+print(data)
---
+++ module-2/python_exceptions.py
@@ -0,0 +1,10 @@
+def divide_by(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError:
+        return 0
+    except Exception as e:
+        print(e, 'Something went wrong!')
+
+ans = divide_by(10,0)
+print(ans)
